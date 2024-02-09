d186fe0112e532f70afd491ef0f4149a4f876e45
João Victor Gregório da Silva <146145021+joaovictorgregorio@users.noreply.github.com>
2024-02-09 16:32:55 -0300

    Repetições em JS parte 1

---
+++ exercicios em aula/aula13/ambiente.js
@@ -0,0 +1,11 @@
+var contador = 1
+
+/* while (contador <= 1009) {
+    console.log(`Passo ${contador}`)
+    contador ++
+} */
+
+do {
+    console.log(`Contando ${contador}`)
+    contador ++
+} while (contador <= 1009)
