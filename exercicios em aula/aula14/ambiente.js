90465dd20e556a99d340133ef74924827a6afda5
João Victor Gregório da Silva <146145021+joaovictorgregorio@users.noreply.github.com>
2024-02-10 17:24:22 -0300

    Repetição FOR conceitos em JS.
    Depuração no Node.js, marcação para inicialização, F5 para iniciar e F10 para avançar passo a passo.

---
+++ exercicios em aula/aula14/ambiente.js
@@ -0,0 +1,7 @@
+for (var contador = 1; contador <= 10; contador++) {
+    console.log('Boa tarde, como vai você? \n')
+}
+
+for (var contador = 1; contador <= 10; contador++) {
+    console.log('Boa noite, como está indo sua vida? \n')
+}
